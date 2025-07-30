b853c3dbd51e7a5c8f731417e22682faddcd43e4
Princess Mercy <mercyadaezeag@gmail.com>
2025-07-30 14:52:07 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
+// tailwind.config.js
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1F1F1F',
+        accent: '#FFD700',
+        neutral: '#F5F5F5',
+        darkText: '#1f2937',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
